5b958a20c483667013de14f2f9febece88a3555b
Andrew Devoe <adevoe86@gmail.com>
2026-06-05 23:30:46 -0400

    setup: tailwind v3 + dna branding

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
     "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
@@ -12,12 +12,11 @@ module.exports = {
         'dna-brown': '#8B6F47',
         'dna-cream': '#F5F1E8',
         'dna-char': '#3D3D3D',
-        'dna-sky': '#7FB3D5',
       },
       fontFamily: {
         display: ['Montserrat', 'sans-serif'],
         body: ['Open Sans', 'sans-serif'],
-      }
+      },
     },
   },
   plugins: [],
